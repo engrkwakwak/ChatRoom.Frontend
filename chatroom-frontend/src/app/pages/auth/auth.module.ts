e43fe7c2ea995f16da2739b0ed2dc43cf962d11c
ph1Bellie <ph1Bellie@obestsolutions.onmicrosoft.com>
2024-05-15 17:41:52 +0800

    Setup Athentication UI: Created a brand name component

--- chatroom-frontend/src/app/pages/auth/auth.module.ts
+++ chatroom-frontend/src/app/pages/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule, NbLayoutModule } from '@nebular/theme';
+import { BrandNameComponent } from './components/brand-name/brand-name.component';
 
 
 
 @NgModule({
   declarations: [
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    BrandNameComponent
   ],
   imports: [
     CommonModule,
